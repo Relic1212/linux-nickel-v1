7b43277fe64f3123a88ecebafb7dcf4d37c566d5
Relic1212 <john.doe@example.com>
2025-03-24 23:37:44 +0100

    fix arm vm script

--- scripts/arm-qemu.sh
+++ scripts/arm-qemu.sh
@@ -1,2 +1,13 @@
+#!/bin/sh
+
 
 # https://translatedcode.wordpress.com/2017/07/24/installing-debian-on-qemus-64-bit-arm-virt-board/
+
+qemu-system-aarch64 \
+	-nographic \
+	-M virt \
+	-m 4G  \
+	-kernel build/tmp/aarch64-image/sysroot/aarch64-unknown-linux-musl/boot/Image   \
+	-append "root=/dev/vda rootwait ro rootfstype=erofs init=/bin/sh   debug "  \
+	-hda build/tmp/aarch64-image/out/destdir/aarch64-root.img \
+	-cpu cortex-a57
