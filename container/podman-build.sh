4446ccd7d9c089c43fddea52f7ae0582bc63d892
Relic1212 <john.doe@example.com>
2025-08-13 08:05:19 +0200

    better

--- container/podman-build.sh
+++ container/podman-build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-podman  build --network host  --file container/arch.Dockerfile  --build-arg groupid=1000 --build-arg username=user -t nickel-arch
+podman  build --network host  --file container/arch.Dockerfile  --build-arg groupid=1000 --build-arg username=user -t nickel-arch $@
