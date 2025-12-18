8f9c636c5f1bae6572bb5707427408ec867634cb
Relic1212 <67271773+Relic1212@users.noreply.github.com>
2025-12-18 16:26:40 +0100

    hyprland wip plugin

--- pkgs/h/hyprland/hyprland_dlopen.c
+++ pkgs/h/hyprland/hyprland_dlopen.c
@@ -5,6 +5,19 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdarg.h>
+
+
+
+#define DLSYM(sym, prefix) 	\
+    if (strcmp(symbol, #sym) == 0) { \
+        extern void* prefix##sym; \
+		dbg_print("(dlsym) found symbol %s with handle=%p, address=%p\n", symbol, handle, &prefix##sym); \
+        return &prefix##sym; \
+    }
+
+
+
+
 static char *dbg;
 static char dbg_initualilised = 0;
 
@@ -40,6 +53,7 @@ const char *lib__usr_lib_libhyprscrolling_so = "lib__usr_lib_libhyprscrolling_so
 const char *lib__usr_lib_libhyprbars_so = "lib__usr_lib_libhyprbars_so";
 const char *lib__usr_lib_libxtra_dispatchers_so = "lib__usr_lib_libxtra_dispatchers_so";
 const char *lib__usr_lib_libhyprfocus_so = "lib__usr_lib_libhyprfocus_so";
+const char *lib__usr_lib_libHyprspace_so = "lib__usr_lib_libHyprspace_so";
 
 void *dlopen(const char *path, int mode)
 {
@@ -76,6 +90,11 @@ void *dlopen(const char *path, int mode)
 	{
 		dbg_print("(dlopen) found library /usr/lib/libhyprfocus.so (handle=%p)\n", &lib__usr_lib_libhyprfocus_so);
 		return &lib__usr_lib_libhyprfocus_so;
+	}
+	if (strcmp(path, "/usr/lib/libHyprspace.so") == 0)
+	{
+		dbg_print("(dlopen) found library /usr/lib/libHyprspace.so (handle=%p)\n", &lib__usr_lib_libHyprspace_so);
+		return &lib__usr_lib_libHyprspace_so;
 	}
 	fprintf(stderr, "(dlopen) WARNING: failed for path %s\n", path);
 	return stub_dlopen(path, mode);
@@ -226,6 +245,31 @@ void *dlsym(void *__restrict handle, const char *__restrict symbol)
 			dbg_print("(dlsym) found symbol pluginInit with handle=%p, address=%p\n", handle, &hyprfocus_pluginInit);
 			return &hyprfocus_pluginInit;
 		}
+	}
+	if (handle == &lib__usr_lib_libHyprspace_so || handle == NULL || handle == &main_program_handle)
+	{
+		if (strcmp(symbol, "pluginAPIVersion") == 0)
+		{
+			extern void *Hyprspace_pluginAPIVersion;
+			dbg_print("(dlsym) found symbol pluginAPIVersion with handle=%p, address=%p\n", handle, &Hyprspace_pluginAPIVersion);
+			return &Hyprspace_pluginAPIVersion;
+		}
+		if (strcmp(symbol, "pluginExit") == 0)
+		{
+			extern void *Hyprspace_pluginExit;
+			dbg_print("(dlsym) found symbol pluginExit with handle=%p, address=%p\n", handle, &Hyprspace_pluginExit);
+			return &Hyprspace_pluginExit;
+		}
+		if (strcmp(symbol, "pluginInit") == 0)
+		{
+			extern void *Hyprspace_pluginInit;
+			dbg_print("(dlsym) found symbol pluginInit with handle=%p, address=%p\n", handle, &Hyprspace_pluginInit);
+			return &Hyprspace_pluginInit;
+		}
+
+		DLSYM(_ZN15CKeybindManager5mouseENSt3__112basic_stringIcNS0_11char_traitsIcEENS0_9allocatorIcEEEE,)
+		DLSYM(_ZN13CHyprRenderer12renderWindowEN9Hyprutils6Memory14CSharedPointerI7CWindowEENS2_I8CMonitorEERKNSt3__16chrono10time_pointINS8_12steady_clockENS8_8durationIxNS7_5ratioILl1ELl1000000000EEEEEEEb15eRenderPassModebb,)
+		DLSYM(_ZN13CHyprRenderer11renderLayerEN9Hyprutils6Memory14CSharedPointerI13CLayerSurfaceEENS2_I8CMonitorEERKNSt3__16chrono10time_pointINS8_12steady_clockENS8_8durationIxNS7_5ratioILl1ELl1000000000EEEEEEEbb,)
 	}
 	fprintf(stderr, "(dlsym) WARNING: failed for symbol %s\n", symbol);
 	return stub_dlsym(handle, symbol);
