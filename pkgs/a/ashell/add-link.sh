e053f912316fe122987f79f4d10b0df681f7dfc6
Relic1212 <67271773+Relic1212@users.noreply.github.com>
2026-02-18 22:02:37 +0100

    ashell: cleanup

--- pkgs/a/ashell/add-link.sh
+++
@@ -1,1 +0,0 @@
-sed -i "s@let libs@println!(\"cargo:rustc-link-lib=static=/tmp/workdir/src/dlopen.o\");\nlet libs@" vendor/libspa-sys/build.rs
--- pkgs/a/ashell/src.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh 
-
-for uri in $(cat uris.txt); do
-    printf "{ src = { uri = \"$uri\",  sha256sum = \"$(url2sha256sum $uri|cut -d ' ' -f1)\", },\ndest = \"\" },\n"
-    
-done
