25d26e594d4756b45d8ba1ed3b2856c2a6ff3cc8
Relic1212 <67271773+Relic1212@users.noreply.github.com>
2026-04-30 11:56:15 +0200

    swaync: fix This commit build a lot of dynamic stuff and is ugly, but at least produces a staic swaync

---
+++ pkgs/s/swaync/swaync_dlopen.c
@@ -0,0 +1,64 @@
+
+#define _GNU_SOURCE
+#include <dlfcn.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+#define DLSYM(sym, prefix) 	\
+    if (strcmp(symbol, #sym) == 0) { \
+        extern void* prefix##sym; \
+		dbg_print("(dlsym) found symbol %s with handle=%p, address=%p\n", symbol, handle, &prefix##sym); \
+        return &prefix##sym; \
+    }
+
+static char* dbg;
+static char dbg_initualilised = 0;
+
+static void dbg_initilialise(){
+	dbg = getenv("DL_DEBUG");
+	dbg_initualilised = 1;
+}
+static void dbg_print(const char *s, ...)
+{
+	if (dbg_initualilised==0){
+		dbg_initilialise();
+	}
+
+    if (dbg != NULL)
+    {
+        va_list args;
+        va_start(args, s);
+        vfprintf(stderr, s, args);
+    }
+}
+
+extern void *stub_dlopen(const char *, int);
+extern void *stub_dlsym(void *__restrict, const char *__restrict);
+extern int stub_dladdr(const void *handle, Dl_info *info);
+
+const char* main_program_handle = "main_program";
+
+const char* lib_ = "lib_";
+
+void* dlopen(const char *path, int mode) {
+	if (path == NULL) { return &main_program_handle; }
+	fprintf(stderr, "(dlopen) WARNING: failed for path %s\n", path);	return stub_dlopen(path, mode);
+}
+
+void* dlsym(void *__restrict handle, const char *__restrict symbol) {
+	if (handle == &lib_ || handle == NULL || handle == &main_program_handle || handle == RTLD_NEXT) { 
+		DLSYM(wl_proxy_destroy, libwayland_)
+		DLSYM(wl_proxy_marshal_array_flags, libwayland_)
+	}
+	fprintf(stderr, "(dlsym) WARNING: failed for symbol %s\n", symbol);	return stub_dlsym(handle, symbol);
+}
+
+
+int dladdr(const void *handle, Dl_info *info)
+{
+    fprintf(stderr, "(dladdr) handle=\"%p\"\n", handle);
+    return stub_dladdr(handle, info);
+}
+
