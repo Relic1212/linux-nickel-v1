adc943ea7ad454ae9114246d0d6ee3bf48642c86
Relic1212 <67271773+Relic1212@users.noreply.github.com>
2026-05-01 16:32:09 +0200

    plasma-common: at least make it build

---
+++ pkgs/k/kde/epoxy_egl.h
@@ -0,0 +1,6 @@
+#define EGL_EGLEXT_PROTOTYPES
+#define GL_GLEXT_PROTOTYPES
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
+#include <GLES2/gl2.h>
+#include <GLES2/gl2ext.h>
---
+++ pkgs/k/kde/epoxy_gl.h
@@ -0,0 +1,5 @@
+#define GL_GLEXT_PROTOTYPES
+#include <GL/gl.h>
+#include <GL/glext.h>
+#include <GLES2/gl2.h>
+#include <GLES2/gl2ext.h>
