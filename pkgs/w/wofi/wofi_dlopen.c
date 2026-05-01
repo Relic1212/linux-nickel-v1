f018964d28957a3f10012f6389fb393e30a7c42c
Relic1212 <67271773+Relic1212@users.noreply.github.com>
2026-05-01 23:56:20 +0200

    wofi: update and fix

---
+++ pkgs/w/wofi/wofi_dlopen.c
@@ -0,0 +1,81 @@
+
+#define _GNU_SOURCE
+#include <dlfcn.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+#define DLSYM(sym, prefix) 	\
+    if (strcmp(symbol, #sym) == 0) { \
+        extern void* prefix##sym; \
+		dbg_print("(dlsym) found symbol %s with handle=%p, address=%p\n", symbol, handle, &prefix##sym); \
+        return &prefix##sym; \
+    }
+
+static char* dbg;
+static char dbg_initualilised = 0;
+
+static void dbg_initilialise(){
+	dbg = getenv("DL_DEBUG");
+	dbg_initualilised = 1;
+}
+static void dbg_print(const char *s, ...)
+{
+	if (dbg_initualilised==0){
+		dbg_initilialise();
+	}
+
+    if (dbg != NULL)
+    {
+        va_list args;
+        va_start(args, s);
+        vfprintf(stderr, s, args);
+    }
+}
+
+extern void *stub_dlopen(const char *, int);
+extern void *stub_dlsym(void *__restrict, const char *__restrict);
+extern int stub_dladdr(const void *handle, Dl_info *info);
+
+const char* main_program_handle = "main_program";
+
+
+void* dlopen(const char *path, int mode) {
+	if (path == NULL) { return &main_program_handle; }
+	fprintf(stderr, "(dlopen) WARNING: failed for path %s\n", path);	return stub_dlopen(path, mode);
+}
+
+void* dlsym(void *__restrict handle, const char *__restrict symbol) {
+ 
+    DLSYM(wofi_dmenu_exec, );
+    DLSYM(wofi_dmenu_get_arg_count, );
+    DLSYM(wofi_dmenu_get_arg_names, );
+    DLSYM(wofi_dmenu_get_widget, );
+    DLSYM(wofi_dmenu_init, );
+    DLSYM(wofi_dmenu_no_entry, );
+
+    DLSYM(wofi_drun_exec, );
+    DLSYM(wofi_drun_get_arg_count, );
+    DLSYM(wofi_drun_get_arg_names, );
+    DLSYM(wofi_drun_get_widget, );
+    DLSYM(wofi_drun_init, );
+    // DLSYM(wofi_drun_no_entry, );
+
+    DLSYM(wofi_run_exec, );
+    DLSYM(wofi_run_get_arg_count, );
+    DLSYM(wofi_run_get_arg_names, );
+    DLSYM(wofi_run_get_widget, );
+    DLSYM(wofi_run_init, );
+    DLSYM(wofi_run_no_entry, );
+
+	fprintf(stderr, "(dlsym) WARNING: failed for symbol %s\n", symbol);	return stub_dlsym(handle, symbol);
+}
+
+
+int dladdr(const void *handle, Dl_info *info)
+{
+    fprintf(stderr, "(dladdr) handle=\"%p\"\n", handle);
+    return stub_dladdr(handle, info);
+}
+
