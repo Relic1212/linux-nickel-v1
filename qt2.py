fc01b0c3f3c6e369a81dda186e5bbd2500e79ace
Relic1212 <67271773+Relic1212@users.noreply.github.com>
2026-05-10 12:52:28 +0200

    qt2 script: wip

--- qt2.py
+++ qt2.py
@@ -7,6 +7,108 @@ import sys
 class Library:
     def __init__(self, path):
         self.path = path
+        self.symbols = self._find_symbols()
+        self._qt_qml()
+
+        self.undefined_symbols = filter(
+            lambda s: s["t"].lower() == "u", self.symbols)
+        self.defined_symbols = filter(
+            lambda s: s["t"].lower() == "t", self.symbols)
+        self.exported_symbols = filter(lambda s: s["t"] == "T", self.symbols)
+        self.other_symbols = filter(lambda s: not (
+            s["t"].lower() in ["u", "t"]), self.symbols)
+
+    def get_defined_symbols(self):
+        return [s["name"] for s in self.defined_symbols]
+
+    def get_exported_symbols(self):
+        return [s["name"] for s in self.exported_symbols]
+
+    def get_undefined_symbols(self):
+        return [s["name"] for s in self.undefined_symbols]
+
+    def get_other_symbols(self):
+        return [s["name"] for s in self.other_symbols]
+    
+    def is_qt_plugin(self):
+        return self.qt_plugin
+    
+    def is_qt_qml_plugin(self):
+        return self.is_qt_qml_plugin
+    
+    def get_imports(self):
+        r = []
+        for s in self.plugin_syms:
+            name = s["name"][21:-1]
+            r.append(f"{self.qt_import_str}({name})")
+        return r
+
+
+    def _qt_qml(self):
+        self.plugin_syms = []
+        plugin = False
+        qml = False
+        for sym in self.symbols:
+            name = sym["name"]
+            # t = sym["t"]
+            if "qt_static_plugin" in name:
+                plugin = True
+                self.plugin_syms.append(sym)
+            if "QQml" in name:
+                qml = True
+            elif "qmlRegisterModule" in name:
+                qml = True
+        self.qt_plugin = plugin 
+        self.qt_qml_plugin = qml
+        if self.qt_plugin:
+            if self.qt_qml_plugin:
+                self.qt_import_str="Q_IMPORT_QML_PLUGIN"
+            else:
+                self.qt_import_str="Q_IMPORT_PLUGIN"
+        else:
+            self.qt_import_str = None
+
+    def _find_symbols(self):
+        try:
+            out = subprocess.run(["llvm-nm", self.path],
+                                 check=True, capture_output=True)
+        except Exception as e:
+            # print(out.stderr.decode())
+            raise e
+
+        s = out.stdout.decode()
+        lines = [l.strip() for l in s.split("\n")]
+        syms = []
+        for line in lines:
+            # print(line)
+            linesplit = line.split()
+            if line == "":
+                continue
+            if len(linesplit) == 1:
+                continue
+            try:
+                # address, t, name = line.split()
+                name = linesplit[-1]
+                t = linesplit[-2]
+            except Exception as e:
+                print(f"failed with line=\"{line}\"")
+                raise e
+
+            syms.append({
+                "name": name,
+                "t": t,
+                "line": line,
+            })
+        return syms
+
+
+def find_files(d):
+    l = []
+    for root, dirs, files in os.walk(d):
+        for f in files:
+            fp = os.path.join(root, f)
+            l.append(fp)
+    return l
 
 
 KNOWN_BAD = ["BuddyEditorPlugin",
@@ -52,48 +154,6 @@ KNOWN_BAD = ["BuddyEditorPlugin",
              ]
 
 
-def find_symbols(lib):
-    try:
-        out = subprocess.run(["llvm-nm", lib], check=True, capture_output=True)
-    except Exception as e:
-        # print(out.stderr.decode())
-        raise e
-
-    s = out.stdout.decode()
-    lines = [l.strip() for l in s.split("\n")]
-    syms = []
-    for line in lines:
-        # print(line)
-        linesplit = line.split()
-        if line == "":
-            continue
-        if len(linesplit) == 1:
-            continue
-        try:
-            # address, t, name = line.split()
-            name = linesplit[-1]
-            t = linesplit[-2]
-        except Exception as e:
-            print(f"failed with line=\"{line}\"")
-            raise e
-
-        syms.append({
-            "name": name,
-            "t": t,
-            "line": line,
-        })
-    return syms
-
-
-def find_files(d):
-    l = []
-    for root, dirs, files in os.walk(d):
-        for f in files:
-            fp = os.path.join(root, f)
-            l.append(fp)
-    return l
-
-
 def find_libs(d):
     files = find_files(d)
     libs = filter(lambda x: x.endswith(".a"), files)
@@ -107,32 +167,10 @@ def t2(d):
     for l in libs:
         assert (l.startswith(d))
         lib_subpath = l[len(d):]
-        qml = False
-        plugin = False
-        syms = find_symbols(l)
-        plugin_syms = []
-        for sym in syms:
-            name = sym["name"]
-            t = sym["t"]
-            if "qt_static_plugin" in name:
-                plugin = True
-                plugin_syms.append(sym)
-            if "QQml" in name:
-                qml = True
-            elif "qmlRegisterModule" in name:
-                qml = True
-        if qml:
-            imp = "Q_IMPORT_QML_PLUGIN"
-        else:
-            imp = "Q_IMPORT_PLUGIN"
-        for p in plugin_syms:
-            name = p["name"]
-            t = p["t"]
-            plugin_name = name[21:-1]
-            if (not plugin_name in KNOWN_BAD) and (t != "U"):
-
-                line = f"{imp}({plugin_name});\t\t// ({t}) {lib_subpath}"
-                clines.append(line)
+        
+        library=Library(l)
+        if library.is_qt_plugin():
+            clines += library.get_imports()
 
     return clines
 
@@ -145,3 +183,6 @@ if __name__ == "__main__":
             lines[i] = "// " + lines[i]
     for l in (lines):
         print(l)
+
+    # lib = Library("build/tmp/musl/out/destdir/usr/lib/libc.a")
+    # print(lib.get_other_symbols())
