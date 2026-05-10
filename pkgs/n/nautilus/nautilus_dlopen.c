4005d228b558a57a79395ef0c4c6c914586bde9d
Relic1212 <67271773+Relic1212@users.noreply.github.com>
2026-05-10 12:49:38 +0200

    nautilus: add still some errors with gtk and egl symbols

--- pkgs/n/nautilus/nautilus_dlopen.c
+++ pkgs/n/nautilus/nautilus_dlopen.c
@@ -40,13 +40,43 @@ extern int stub_dladdr(const void *handle, Dl_info *info);
 
 const char* main_program_handle = "main_program";
 
+const char* lib__usr_lib_tinysparql_3_0_libtracker_http_soup3_so = "lib__usr_lib_tinysparql_3_0_libtracker_http_soup3_so";
+const char* lib__usr_lib_tinysparql_3_0_libtracker_parser_libicu_so = "lib__usr_lib_tinysparql_3_0_libtracker_parser_libicu_so";
 
 void* dlopen(const char *path, int mode) {
 	if (path == NULL) { return &main_program_handle; }
+	if (strcmp( path, "/usr/lib/tinysparql-3.0/libtracker-http-soup3.so" ) == 0) { dbg_print("(dlopen) found library /usr/lib/tinysparql-3.0/libtracker-http-soup3.so (handle=%p)\n", &lib__usr_lib_tinysparql_3_0_libtracker_http_soup3_so); return &lib__usr_lib_tinysparql_3_0_libtracker_http_soup3_so; }
+	if (strcmp( path, "/usr/lib/tinysparql-3.0/libtracker-parser-libicu.so" ) == 0) { dbg_print("(dlopen) found library /usr/lib/tinysparql-3.0/libtracker-parser-libicu.so (handle=%p)\n", &lib__usr_lib_tinysparql_3_0_libtracker_parser_libicu_so); return &lib__usr_lib_tinysparql_3_0_libtracker_parser_libicu_so; }
 	fprintf(stderr, "(dlopen) WARNING: failed for path %s\n", path);	return stub_dlopen(path, mode);
 }
 
 void* dlsym(void *__restrict handle, const char *__restrict symbol) {
+	if (handle == &lib__usr_lib_tinysparql_3_0_libtracker_http_soup3_so || handle == NULL || handle == &main_program_handle) { 
+		DLSYM(initialize_types, tracker_http_soup3__)
+		DLSYM(print_parameter_entry, tracker_http_soup3__)
+		DLSYM(tracker_get_debug_flags, tracker_http_soup3__)
+		DLSYM(tracker_http_client_get_type, tracker_http_soup3__)
+		DLSYM(tracker_http_client_soup_get_type, tracker_http_soup3__)
+		DLSYM(tracker_http_server_get_type, tracker_http_soup3__)
+		DLSYM(tracker_http_server_soup_get_type, tracker_http_soup3__)
+	}
+	if (handle == &lib__usr_lib_tinysparql_3_0_libtracker_parser_libicu_so || handle == NULL || handle == &main_program_handle) { 
+		DLSYM(tracker_collation_init, tracker_parser_icu__)
+		DLSYM(tracker_collation_shutdown, tracker_parser_icu__)
+		DLSYM(tracker_collation_utf8, tracker_parser_icu__)
+		DLSYM(tracker_parser_casefold, tracker_parser_icu__)
+		DLSYM(tracker_parser_free, tracker_parser_icu__)
+		DLSYM(tracker_parser_new, tracker_parser_icu__)
+		DLSYM(tracker_parser_next, tracker_parser_icu__)
+		DLSYM(tracker_parser_normalize, tracker_parser_icu__)
+		DLSYM(tracker_parser_reset, tracker_parser_icu__)
+		DLSYM(tracker_parser_tolower, tracker_parser_icu__)
+		DLSYM(tracker_parser_toupper, tracker_parser_icu__)
+		DLSYM(tracker_parser_unaccent, tracker_parser_icu__)
+		DLSYM(tracker_language_get_type, tracker_parser_icu__)
+		DLSYM(tracker_language_new, tracker_parser_icu__)
+		DLSYM(tracker_language_stem_word, tracker_parser_icu__)
+	}
 	fprintf(stderr, "(dlsym) WARNING: failed for symbol %s\n", symbol);	return stub_dlsym(handle, symbol);
 }
 
